50a4fa31dea1e6ddd69f83fd5696fb21c016fe3f
Djurica Djuricic <p3ntaphr3ak@gmail.com>
2018-03-20 00:47:49 +0100

    DZ02DZ03

---
+++ dz02/dz02.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl -w
+use warnings;
+#ovaj program stampa sve unesene vrednosti
+
+print "Unesite vrednost:\n";
+$input = <STDIN>;
+chomp($input);
+if($input eq "\n")
+{
+	print "Niste uneli vrednost...\n";
+}
+else
+{
+	@lista = ("$input");
+	while(!($input eq "\n"))
+	{
+		print "Unesite vrednost:\n";
+		$input = <STDIN>;
+		if($input eq "\n")
+		{
+			last;
+		}
+		chomp($input);
+		push @lista, "$input";
+	}
+	print "Niz unesenih vrednosti:\n";
+	foreach(@lista)
+	{
+		print "'$_', ";
+	}
+}
+
---
+++ dz03/dz03.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl -w
+use warnings;
+
+#stampanje kvadrata brojeva (range operator)
+@brojevi = (1..10);
+print("\n Niz kvadrata brojeva od 1 do 10: \n");
+foreach $broj (@brojevi)
+{
+	print("$broj" * "$broj".", ");
+	#print("sta");
+
+
+#stampanje kvadrata brojeva (shift funkcija)
+print("\n Niz kvadrata brojeva od 1 do 10 (via shift): \n");
+@array = (1..10);
+while($element = shift(@array))
+{
+	print("$element" * "$element".", ");
+}
+
+
+#obrtanje unosa korisnika
+print("\n Unesite recenicu za obradu: \n");
+$input = <STDIN>;
+chomp($input);
+$result = reverse $input;
+print("Obrnuta recenica :\n $result \n");
